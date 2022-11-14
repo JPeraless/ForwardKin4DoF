c52025dfe042d52fea1757c8cc82fad904b1e091
JPeraless <jalfperi@hotmail.com>
2022-11-13 18:30:35 -0700

    Create inverseKin
    Created inverseKin.py

---
+++ inverseKin.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+############################################################
+# Inverse Kinematics
+# Get to specify where we want the end-effector to be 
+# and calculate the angles needed to get to that position.
+############################################################
+
+
