a519516d0609216cd12d3603b72c38fe39f59503
JPeraless <jalfperi@hotmail.com>
2022-11-16 17:27:31 -0700

    Inputted values to test the equations

--- inverseKin.py
+++ inverseKin.py
@@ -26,11 +26,11 @@ Thus, one cannot expect to make use of the fundamental IK assumptions.
 def main():
     
     # Set link lengths
-    a1 = 0; a2 = 0; a3 = 0; a4 = 0; a5 = 0
+    a1 = 25.0; a2 = 2.0; a3 = 10.0; a4 = 5.0; a5 = 2.0
 
     # Desired end-effector coordinates
-    x = 0
-    y = 0
+    x = 5.0
+    y = 5.0
 
     # Height from frame 0 to 2
     z0_2 = 0  # not sure how to get this
@@ -48,32 +48,43 @@ def main():
     phi1 = acos(((a3 * a3) + (a4 + a5) - (k * k)) / 2 * a3 * (a4 + a5))
 
     # Joint variable equations
-    theta1 = atan(x / y)
-    theta2 = atan(r2 / r1)  # asin(r2 / a3) should also work
-    theta3 = 180 - phi1
+    theta_1 = atan(y / x)  # angle of the first joint
+    theta_2 = atan(r2 / r1)  # asin(r2 / a3) should also work
+    theta_3 = 180 - phi1
+
+    print(f"Theta 1 = {theta_1} radians\n")
+    print(f"Theta 2 = {theta_2} radians\n")
+    print(f"Theta 3 = {theta_3} radians\n")
+
+    finalRotation(theta_1, theta_2, theta_3)
+
 
     # Use to test correctness of k (needs phi1 to be known)
-    kTest = sqrt((a3 * a3) + (a4 + a5) - 2 * a3 * (a4 + a5) * np.cos(phi1))
+    # kTest = sqrt((a3 * a3) + (a4 + a5) - 2 * a3 * (a4 + a5) * np.cos(phi1))
 
-def finalRotation(theta1, theta2, theta3):
-    r = theta1; s = theta2; t = theta3
 
-    # Desired end-effector rotation on base frame
+# Calculate the rotation required to get the desired rotation
+def finalRotation(theta_1, theta_2, theta_3):
+    r = theta_1; s = theta_2; t = theta_3
+
+    # Desired orientation of end-effector relative to the base frame
     r0_4 = [
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]
+        [-1.0, 0.0, 0.0],
+        [0.0, -1.0, 0.0],
+        [0.0, 0.0, -1.0]
     ]
 
     # Rotation matrix frame 3 on 0
     r0_3 = [
-        [-cos(r)*sin(s)*cos(t) - cos(r)*cos(s)*sin(t),    0,           cos(r)*cos(s)*cos(t) - cos(r)*sin(s)*sin(t)],
-        [sin(r)*sin(s)*(-cos(t)) - sin(r)*cos(s)*sin(t),  -cos(r),     sin(r)*cos(s)*cos(t) - sin(r)*sin(s)*sin(t)],
-        [cos(s)*cos(t) - sin(s)*sin(t),                 0,           sin(s)*cos(t) + cos(s)*sin(t)]
+        [-cos(r)*sin(s)*cos(t) - cos(r)*cos(s)*sin(t),      0,           cos(r)*cos(s)*cos(t) - cos(r)*sin(s)*sin(t)],
+        [sin(r)*sin(s)*(-cos(t)) - sin(r)*cos(s)*sin(t),    -cos(r),     sin(r)*cos(s)*cos(t) - sin(r)*sin(s)*sin(t)],
+        [cos(s)*cos(t) - sin(s)*sin(t),                     0,           sin(s)*cos(t) + cos(s)*sin(t)]
     ]
 
+    # Calculate the inverse of r0_3
     invR0_3 = np.linalg.inv(r0_3)
 
+    # Calculate the rotation matrix of frame 4 on 3
     r3_4 = np.dot(invR0_3, r0_4)
 
     print("\n===== r3_4 =====")
@@ -81,11 +92,11 @@ def finalRotation(theta1, theta2, theta3):
     print("\n================")
 
     # Test rotation matrix for frame 4 on 3
-    testR3_4 = [
+    '''testR3_4 = [
         [cos(u),    -sin(u),    0],
         [sin(u),    cos(u),     0],
         [0,         0,          1]
-    ]
+    ]'''
 
 
 if __name__ == "__main__":
